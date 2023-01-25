00f7b88d874708c0cf5508de6138262e43a3876d
Agustin <90871968+Ler0v@users.noreply.github.com>
2023-01-25 16:37:53 -0300

    Base

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='section'>
+        <h1 className='title'>Agustin</h1>
+        <h1 className='title'>Robledo</h1>
+        <p className='parr'>Web designer & Developer</p>
+      </div>
+      <div className='section2'>
+        <h1 className='title'>Sobre Mi</h1>
+      </div>
+      <div className='section'>
+        <h1 className='title'>Mis Habilidades</h1>
+      </div>
+      <div className='section2'>
+        <h1 className='title'>Contacto</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
